8246495e2a1d3b7a375c2470db02c11f1e3e706f
Brigette LeVert <bnl.levert@gmail.com>
2019-09-08 19:12:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 
 def remove_element_from_end_of_array(array)
   #array_remove1 = remove_element_from_end_of_array.pop
-  array.shift
+  array.shift("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
