3aceeb38133fca75680b24e22fff781963226838
Brigette LeVert <bnl.levert@gmail.com>
2019-09-08 19:07:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("array" , "arrays!") 
+  #add_element_to_end_of_array.push("array" , "arrays!") 
   #add_element_to_end_of_array << array
   #add_element_to_end_of_array << element
   #create_an_array.push("array", "element")
+  array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
